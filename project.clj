b9233b390a7c31f8f87e946fbe52cf6e223bd747
Chas Lemley <chas.lemley@gmail.com>
2010-10-28 09:37:27 -0400

    updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slim "0.1.0"
+(defproject slim "0.1.1"
   :description "Simple HTML Templating for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
