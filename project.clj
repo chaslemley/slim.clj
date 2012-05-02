96523806903dcd19d37f64e5735132b5c1b5ab26
Devin Walters <devinw@gmail.com>
2012-05-01 22:23:35 -0500

    Update clojure, hiccup dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject slim "0.1.3"
+(defproject slim "0.1.4"
   :description "Simple HTML Templating for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [hiccup "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccup "1.0.0"]])
