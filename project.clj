3e39df8c4912bee55c8fdac8e5d3b51f539e3b0f
Devin Walters <devinw@gmail.com>
2012-05-17 20:31:58 -0500

    Updates project.clj to match lein2 style

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject slim "0.1.4"
+(defproject slim "0.1.5"
   :description "Simple HTML Templating for Clojure"
+  :url "http://github.com/devn/slim.clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [hiccup "1.0.0"]])
